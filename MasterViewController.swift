7354b50a20a51dd4ae60fa45ae41163e3fecdde1
jonathansayer <jon.sayer1@gmail.com>
2015-12-17 15:36:54 +0000

    setup of cocoa applicatio

---
+++ MasterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.swift
+//  ScaryBugsMac
+//
+//  Created by Jonathan Sayer on 17/12/2015.
+//  Copyright © 2015 my.company. All rights reserved.
+//
+
+import Cocoa
+
+class MasterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ ScaryBugsMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ScaryBugsMac
+//
+//  Created by Jonathan Sayer on 17/12/2015.
+//  Copyright © 2015 my.company. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
